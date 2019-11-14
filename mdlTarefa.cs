a1e0d32deb76395f6d5534634f06e317b31ad668
Wellington Mazoni de Andrade <TBF03122@tbforte.tbf>
2019-11-14 08:48:18 -0300

    Adicionando base de dados

--- mdlTarefa.cs
+++ mdlTarefa.cs
@@ -4,7 +4,6 @@ namespace CamadaModelo
 {
     public class mdlTarefa
     {
-        public int ID { get; set; }
         public string Tarefa { get; set; }
         public DateTime Prazo { get; set; }
         public string Atividade { get; set; }
